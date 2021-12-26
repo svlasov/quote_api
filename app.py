6556098d0eb69bdddde21ab4e10e9464fd2a742c
Sergey Vlasov <im.sergey.vlasov@gmail.com>
2021-12-26 21:40:42 +0200

    add query string parsing

--- app.py
+++ app.py
@@ -1,11 +1,37 @@
-from flask import Flask
+import pprint
+from operator import itemgetter
+
+from flask import Flask, request
+from flask_api import status
+from werkzeug.exceptions import BadRequestKeyError
 
 app = Flask(__name__)
+logger = app.logger
+
+# Method: GET; Request URL: <BASE_URL>/api/quote
+@app.route('/api/quote')
+def quote():
+    logger.debug(pprint.pformat(request.args))
+
+    param_names = "from_currency_code", "amount", "to_currency_code"
+
+    extract_quote_params = itemgetter(*param_names)
+
+    try:
+        from_currency_code, amount, to_currency_code = extract_quote_params(request.args)
+    except BadRequestKeyError as ex:
+        # return Bad Request response but don't show the stack trace
+        return f"Expected to get params: {param_names}", status.HTTP_400_BAD_REQUEST
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+    return {
+        "from_currency_code": from_currency_code,
+        "to_currency_code": to_currency_code,
+        'exchange_rate': None,
+        'currency_code': None,
+        'amount': amount,
+        'provider_name': ''
+    }
 
 
 if __name__ == '__main__':
